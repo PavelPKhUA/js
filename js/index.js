1e178fbc40db8d1a93e98e321bd3013825e41ae3
PavelPKhUA <PavelPKhUA@gmail.com>
2023-08-29 15:09:09 +0300

    lesson 10

--- js/index.js
+++ js/index.js
@@ -801,3 +801,91 @@ const calcSpaceFromDesign = (componentName) => {
 }
 
 console.log(calcSpaceFromDesign('card'))
+
+// Lesson 11
+console.log(' ')
+console.log('Lesson 11')
+
+//eval eval(script)
+let s = 10
+let t = 20
+let code = 'console.log(s + t)'
+eval(code)
+
+const code2 = '(5 + 5) % 5 === 0'
+console.log(eval(code2))
+
+const code3 = `console.log('Hello')`
+eval(code3)
+
+//isFinite
+function calcScreenRation(w, h) {
+  let result = w / h
+  return isFinite(result) ? result : null
+}
+console.log(calcScreenRation(1920, '1080'))
+
+//isNaN
+
+function calcScreenRation(w, h) {
+  let result = w / h
+  if (isNaN(result)) {
+    return 'Error'
+  } else if (!isFinite(result)) {
+    return 'Is Infinity'
+  } else {
+    return result
+  }
+}
+console.log(calcScreenRation(1920, '1080'))
+
+//parseInt
+
+const num = parseInt('0101', 2) // після коми вказується система (двійкова), з якої треба робити перетворення у ціле число
+console.log(num)
+
+function calcScreenRation(w, h) {
+  w = parseInt(w)
+  h = parseInt(h)
+  let result = w / h
+  if (isNaN(result)) {
+    return 'Error'
+  } else if (!isFinite(result)) {
+    return 'Is Infinity'
+  } else {
+    return result
+  }
+}
+console.log(calcScreenRation('1920px', '1080px'))
+
+//parseFloat
+const num2 = parseFloat('1.5555')
+console.log(num2)
+
+//URI
+// encodeURI
+// encodeURIComponent
+// decodeURI
+// decodeURIComponent
+const domain = 'it-brains.com.ua'
+function redirectToPath(path) {
+  const link = encodeURI(`https://${domain}/${path}`)
+  // робимо перехід на сторінку
+  console.log(link)
+  return link
+}
+
+const URI =
+  'https://it-brains.com.ua/product/%D0%9C%D1%96%D0%BA%D1%80%D0%BE%D1%84%D0%BE%D0%BD%20%D1%80%D0%BE%D0%B6%D0%B5%D0%B2%D0%B8%D0%B9/info'
+function getUrl(urlInURI) {
+  urlInURI = decodeURI(urlInURI)
+  console.log(urlInURI)
+  //   return urlInURI
+}
+
+const path = 'product/Мікрофон рожевий/info'
+const link = `https://${domain}/${path}`
+let link1 = redirectToPath(path)
+let link2 = getUrl(link1)
+
+// console.log(link === link2)
